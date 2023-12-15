270ee25d65a08ef690d81639a01e6d784552c9f2
Gabe Bartek <gabebartek20@gmail.com>
2023-12-15 16:56:18 -0600

    BossFight.5

--- Assets/Scripts/MinotaurBossFight.cs
+++ Assets/Scripts/MinotaurBossFight.cs
@@ -5,17 +5,96 @@ using UnityEngine;
 public class MinotaurBossFight : MonoBehaviour
 {
 
+    public float stunTime = 2.0f;
+    public float chargeInterval = 1.0f;
+    public float speed = 3.0f;
+    public float chargeSpeed = 6.0f;
 
+    private bool isCharging = false;
+
+    Rigidbody2D minotaurBody;
+    private Vector2 playerSnapshot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        minotaurBody = GetComponent<Rigidbody2D>();
+        InvokeRepeating("StartCharge", chargeInterval, chargeInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isCharging)
+        {
+            Vector2 direction = (playerSnapshot - (Vector2)transform.position).normalized;
+            minotaurBody.velocity = direction * chargeSpeed;
+        }
         
+    }
+
+    private void StartCharge()
+    {
+        Invoke("TakePlayerSnapshot", 5);
+    }
+
+    private void TakePlayerSnapshot()
+    {
+        // Store the player's position as a snapshot
+        playerSnapshot = (Vector2)GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        // Start the charge attack
+        isCharging = true;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the boss collides with a wall
+        if (other.CompareTag("Pillar"))
+        {
+            // Stun the boss when it hits the wall
+            StunBoss();
+        }
+    }
+
+
+    private void StunBoss()
+    {
+        isCharging = false;
+        StartCoroutine(StunCoroutine());
+    }
+
+    private IEnumerator StunCoroutine()
+    {
+        // Add any visual or audio feedback for the stun
+        Debug.Log("Boss stunned!");
+
+        GetComponent<SpriteRenderer>().color = Color.red;
+        minotaurBody.velocity = Vector2.zero; // Stop the boss's movement
+        yield return new WaitForSeconds(stunTime);
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        // Resume charging after the stun duration
+        isCharging = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+        isCharging = false;
+
+
+        if (player != null)
+        {
+            Debug.Log("You got hit!");
+        }
+
+        // Check if the boss collides with a wall
+        if (collision.gameObject.CompareTag("Pillar"))
+        {
+            // Stun the boss when it hits the wall
+            StunBoss();
+        }
     }
 }
